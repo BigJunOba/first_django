055e48b85fa51217796643f9a761e05df775a25f
BigJunOba <sjy534948129@sina.com>
2019-04-16 16:25:58 +0800

    实现文章详情页上下篇文章跳转

--- blog/views.py
+++ blog/views.py
@@ -39,16 +39,35 @@ def get_index_page(request):
 def get_detail_page(request, article_id):
     all_article = Article.objects.all()
     curr_article = None
-    for article in all_article:
+    previous_index = 0
+    next_index = 0
+    previous_article = None
+    next_article = None
+    for index, article in enumerate(all_article):  # 使用迭代器取出当前文章的下标
+        if index == 0 :
+            previous_index = 0
+            next_index = index + 1
+        elif index == len(all_article) - 1:
+            previous_index = index - 1
+            next_index = index
+        else:
+            previous_index = index - 1
+            next_index = index + 1
+
         if article.article_id == article_id:
             curr_article = article
+            previous_article = all_article[previous_index]
+            next_article = all_article[next_index]
             break
+
+
 
     section_list = curr_article.content.split('\n')
     return render(request, 'detail.html',
                   {
                       'curr_article': curr_article,
-                      'section_list': section_list
+                      'section_list': section_list,
+                      'previous_article': previous_article,
+                      'next_article': next_article
                   }
                   )
-
