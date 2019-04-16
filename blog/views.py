a25a20a46825d0a6a95409be6a2cc71b230a728a
BigJunOba <sjy534948129@sina.com>
2019-04-16 17:02:01 +0800

    使用Paginator实现分页功能

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 from blog.models import Article
 
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
@@ -28,10 +30,37 @@ def article_content(request):
 
 
 def get_index_page(request):
+    page = request.GET.get('page')
+    if page:
+        page = int(page)
+    else:
+        page = 1
+
     all_article = Article.objects.all()
+
+    paginator = Paginator(all_article, 4)
+    page_num = paginator.num_pages
+    print('page num', paginator.num_pages)
+
+    page_article_list = paginator.page(page)
+
+    if page_article_list.has_next():
+        next_page = page + 1
+    else:
+        next_page = page
+
+    if page_article_list.has_previous():
+        previous_page = page - 1
+    else:
+        previous_page = page
+
     return render(request, 'index.html',
                   {
-                      'article_list': all_article
+                      'article_list': page_article_list,
+                      'page_num': range(1, page_num + 1),
+                      'curr_page': page,
+                      'next_page': next_page,
+                      'previous': previous_page,
                   }
                   )
 
@@ -44,7 +73,7 @@ def get_detail_page(request, article_id):
     previous_article = None
     next_article = None
     for index, article in enumerate(all_article):  # 使用迭代器取出当前文章的下标
-        if index == 0 :
+        if index == 0:
             previous_index = 0
             next_index = index + 1
         elif index == len(all_article) - 1:
@@ -59,8 +88,6 @@ def get_detail_page(request, article_id):
             previous_article = all_article[previous_index]
             next_article = all_article[next_index]
             break
-
-
 
     section_list = curr_article.content.split('\n')
     return render(request, 'detail.html',
